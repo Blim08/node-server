9f33f0de234b2812b22b63303b9d2851f6824f0e
Jairo Durango <cartuchoxl@hotmail.com>
2023-06-23 00:27:27 -0500

    readme

--- servidor.js
+++ servidor.js
@@ -1,25 +1,117 @@
-const http = require('http');
-// http req resp
-const server = http.createServer((req, res) => {
-  
-  res.setHeader('Content-Type', 'application/json');
+const readline = require('readline');
 
-  // Array de tareas
-  const tareas = [
-    { id: 1, nombre: 'Cenar con mis padres', descripcion: 'Ir a un restaurante y compartir una cena en familia', completado: false },
-    { id: 2, nombre: 'Pasear a mis perros', descripcion: 'Llevar a mis perros a dar un paseo en el parque', completado: true },
-    { id: 3, nombre: 'Lavar la moto', descripcion: 'Limpiar a fondo la moto y pulir el exterior', completado: false }
-  ];
-
-  // Convertir el arreglo de tareas a JSON
-  const tareasJson = JSON.stringify(tareas);
-
-  // Enviar las respuesta con el arreglo de tareas en formato JSON
-  res.end(tareasJson);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-const puerto = 3000;
+let taskList = [];
 
-server.listen(puerto, () => {
-  console.log(`Servidor iniciado en http://localhost:${puerto}`);
-});
+function addTask() {
+    return new Promise((resolve, reject) => {
+        rl.question('Introduce la descripción de la tarea: ', (description) => {
+            if (description.trim() === '') {
+                reject(new Error('La descripción de la tarea no puede estar vacía.'));
+            } else {
+                const task = {
+                    id: taskList.length + 1,
+                    description: description,
+                    completed: false
+                };
+                taskList.push(task);
+                console.log(`La tarea "${task.description}" ha sido añadida a la lista.`);
+                resolve();
+            }
+        });
+    });
+}
+
+function removeTask() {
+    return new Promise((resolve, reject) => {
+        rl.question('Introduce el número de la tarea que deseas eliminar: ', (taskId) => {
+            const taskIndex = taskList.findIndex(task => task.id === parseInt(taskId));
+            if (taskIndex !== -1) {
+                const task = taskList[taskIndex];
+                taskList.splice(taskIndex, 1);
+                console.log(`La tarea "${task.description}" ha sido eliminada de la lista.`);
+                resolve();
+            } else {
+                reject(new Error('No se encontró la tarea con el número indicado.'));
+            }
+        });
+    });
+}
+
+function completeTask() {
+    return new Promise((resolve, reject) => {
+        rl.question('Introduce el número de la tarea que deseas marcar como completada: ', (taskId) => {
+            const taskIndex = taskList.findIndex(task => task.id === parseInt(taskId));
+            if (taskIndex !== -1) {
+                const task = taskList[taskIndex];
+                task.completed = true;
+                console.log(`La tarea "${task.description}" ha sido marcada como completada.`);
+                resolve();
+            } else {
+                reject(new Error('No se encontró la tarea con el número indicado.'));
+            }
+        });
+    });
+}
+
+function showTasks() {
+    console.log('Lista de tareas:');
+    taskList.forEach(task => {
+        console.log(`[${task.id}] ${task.description} - ${task.completed ? 'Completada' : 'Pendiente'}`);
+    });
+    return Promise.resolve();
+}
+
+async function showMenu() {
+    console.log('\n¿Qué acción deseas realizar?');
+    console.log('[1] Añadir una tarea');
+    console.log('[2] Eliminar una tarea');
+    console.log('[3] Marcar una tarea como completada');
+    console.log('[4] Mostrar la lista de tareas');
+    console.log('[5] Salir');
+    rl.question('Opción seleccionada: ', async (option) => {
+        switch (option) {
+            case '1':
+                try {
+                    await addTask();
+                } catch (error) {
+                    console.log(error.message);
+                }
+                showMenu();
+                break;
+            case '2':
+                try {
+                    await removeTask();
+                } catch (error) {
+                    console.log(error.message);
+                }
+                showMenu();
+                break;
+            case '3':
+                try {
+                    await completeTask();
+                } catch (error) {
+                    console.log(error.message);
+                }
+                showMenu();
+                break;
+            case '4':
+                await showTasks();
+                showMenu();
+                break;
+            case '5':
+                rl.close();
+                break;
+            default:
+                console.log('Opción no válida.');
+                showMenu();
+                break;
+        }
+    });
+}
+
+showMenu();
